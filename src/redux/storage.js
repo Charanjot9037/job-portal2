85a1c069f2821f7ae9f3b154177575807254c527
charanjot singh <charanjot2005@gmail.com>
2025-08-06 22:03:32 +0530

    commit

--- src/redux/storage.js
+++ src/redux/storage.js
@@ -1,38 +1,21 @@
-// // utils/createStorage.js
-// import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+// utils/createStorage.js
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
-// const createNoopStorage = () => {
-//   return {
-//     getItem(_key) {
-//       return Promise.resolve(null);
-//     },
-//     setItem(_key, value) {
-//       return Promise.resolve(value);
-//     },
-//     removeItem(_key) {
-//       return Promise.resolve();
-//     },
-//   };
-// };
-
-// const storage =
-//   typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
-
-// export default storage;
-// src/redux/storage.js
-
-let storage;
-
-if (typeof window !== 'undefined') {
-  // Only require on client side
-  storage = require('redux-persist/lib/storage').default;
-} else {
-  // Noop for server
-  storage = {
-    getItem: () => null,
-    setItem: () => {},
-    removeItem: () => {},
+const createNoopStorage = () => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key) {
+      return Promise.resolve();
+    },
   };
-}
+};
+
+const storage =
+  typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
 
 export default storage;
