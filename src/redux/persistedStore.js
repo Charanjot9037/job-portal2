bf4c803b100de62df8bb9168ef4119bbe989480f
charanjot singh <charanjot2005@gmail.com>
2025-08-06 23:07:21 +0530

    commit

--- src/redux/persistedStore.js
+++ src/redux/persistedStore.js
@@ -1,4 +1,50 @@
+// // redux/persistedStore.js
+// import { configureStore, combineReducers } from "@reduxjs/toolkit";
+// import authSlice from './authSlice.js';
+// import jobSlice from './jobSlice.js';
+// import companySlice from './companySlice.js';
+// import ApplicationSlice from './applicationslice.js';
 
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+
+// import storage from 'redux-persist/lib/storage';
+
+// const persistConfig = {
+//   key: 'root',
+//   version: 1,
+//   storage,
+// };
+
+// const rootReducer = combineReducers({
+//   auth: authSlice,
+//   job: jobSlice,
+//   company: companySlice,
+//   Application: ApplicationSlice,
+// });
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export const persistor = persistStore(store);
+// redux/persistedStore.js
 
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authSlice from './authSlice.js';
@@ -50,12 +96,5 @@ export const store = configureStore({
 });
 
 // Export a fallback persistor
-// export const persistor =
-//   typeof window !== 'undefined' ? persistStore(store) : { purge: () => {}, flush: () => {} };
-let persistor;
-
-if (typeof window !== 'undefined') {
-  persistor = persistStore(store);
-}
-
-export { store, persistor };
+export const persistor =
+  typeof window !== 'undefined' ? persistStore(store) : { purge: () => {}, flush: () => {} };
