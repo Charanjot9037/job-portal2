3037c6750f4021258f410b45f627d571c06be473
charanjot singh <charanjot2005@gmail.com>
2025-08-06 22:46:51 +0530

    commit

--- src/app/ClientLayout.jsx
+++ src/app/ClientLayout.jsx
@@ -5,13 +5,12 @@ import dynamic from 'next/dynamic';
 import ProgressBar from './components/progressbar';
 import Navbar from './components/shared/Navbar';
 import { Toaster } from 'react-hot-toast';
+import ProvidersWrapper from '@/app/providers/ProvidersWrapper';
 
 // const ProvidersWrapper = dynamic(() => import('@/app/providers/ProvidersWrapper'), {
 //   ssr: false,
 // });
-const ProvidersWrapper = dynamic(() => import('@/app/providers/ProvidersWrapper.js'), {
-  ssr: false,
-});
+
 
 export default function ClientLayout({ children }) {
   return (
