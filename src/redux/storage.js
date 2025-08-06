6bfb11bf55b024ba7f8e106691716cc1a007ce98
charanjot singh <charanjot2005@gmail.com>
2025-08-06 19:30:13 +0530

    commit

--- src/redux/storage.js
+++ src/redux/storage.js
@@ -1,5 +1,28 @@
-// utils/createStorage.js
-import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+// // utils/createStorage.js
+// import {createWebStorage} from "redux-persist/lib/storage/createWebStorage";
+
+// const createNoopStorage = () => {
+//   return {
+//     getItem(_key) {
+//       return Promise.resolve(null);
+//     },
+//     setItem(_key, value) {
+//       return Promise.resolve(value);
+//     },
+//     removeItem(_key) {
+//       return Promise.resolve();
+//     },
+//   };
+// };
+
+// const storage =
+//   typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
+
+// export default storage;
+// ✅ FIXED: Correct way to import createWebStorage
+import * as storageModule from 'redux-persist/lib/storage/createWebStorage';
+
+const createWebStorage = storageModule.default || storageModule.createWebStorage;
 
 const createNoopStorage = () => {
   return {
